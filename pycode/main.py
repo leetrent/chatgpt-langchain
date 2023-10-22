58b09c9ffe1d1d618668d6829de99c639cd5528f
LeeTrent <lee.trent.1@gmail.com>
2023-10-22 10:25:36 -0500

    Chainged 2 prompts together

--- pycode/main.py
+++ pycode/main.py
@@ -1,29 +1,52 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
+
+load_dotenv()
+llm = OpenAI()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--language", default="Python")
 parser.add_argument("--task", default="Return a list of numbers.")
 args = parser.parse_args()
 
-load_dotenv()
-llm = OpenAI()
-
 code_prompt = PromptTemplate (
     template="Write a very short {language} function that will {task}.",
     input_variables=["language", "task"]
 )
 
-code_chain = LLMChain (
-    llm=llm,
-    prompt=code_prompt
+test_prompt = PromptTemplate (
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} code:\n{code}."
+
 )
 
-result = code_chain ({
+code_chain = LLMChain (
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+combined_chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = combined_chain ({
     "language": args.language,
     "task": args.task
 })
-print(result["text"])
+
+print("\n\n>>>>>>Generated Code:")
+print(result["code"])
+print("\n\n>>>>>Generated Test:")
+print(result["test"])
