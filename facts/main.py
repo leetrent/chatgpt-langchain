ca9f9b2b51145d03b8782d3dc81a4fe35025ee9e
LeeTrent <lee.trent.1@gmail.com>
2023-11-06 08:06:27 -0600

    Just getting started using Chromadb

--- facts/main.py
+++ facts/main.py
@@ -1,6 +1,8 @@
 
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,16 +11,36 @@ text_splitter = CharacterTextSplitter(
     separator="\n",
     chunk_size=200,
     chunk_overlap=0
-
 )
+
 
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-#print(docs)
-for doc in docs:
-    print(doc.page_content)
+embeddings = OpenAIEmbeddings()
+
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+    
+)
+
+# results = db.similarity_search_with_score("What is an interesting fact about the English language?")
+
+# for result in results:
+#     print("\n")
+#     print(result[1])
+#     print(result[0].page_content)
+
+results = db.similarity_search(
+    "What is an interesting fact about the English language?",
+    k=1
+)
+
+for result in results:
     print("\n")
+    print(result.page_content)
 
